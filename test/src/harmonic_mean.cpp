d37c5c9eb08e22c088f873849e8c6961633395d8
biowpn <ymenghank@gmail.com>
2023-03-11 15:07:31 +0800

    assert_almost_equal

--- test/src/harmonic_mean.cpp
+++ test/src/harmonic_mean.cpp
@@ -10,14 +10,14 @@ using namespace xstd;
 void test_harmonic_mean()
 {
     std::array x{40.0, 60.0};
-    assert_equal(std::round(harmonic_mean(x)), 48.0);
+    assert_almost_equal(harmonic_mean(x), 48.0);
 }
 
 void test_weighted_harmonic_mean()
 {
     std::array x{40.0, 60.0};
     std::array w{5.0, 30.0};
-    assert_equal(std::round(weighted_harmonic_mean(x, w)), 56.0);
+    assert_almost_equal(weighted_harmonic_mean(x, w), 56.0);
 }
 
 auto main() -> int
