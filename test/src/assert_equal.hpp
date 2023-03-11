f85210c15f9b3942172d4a00a76e7378b7fef933
biowpn <ymenghank@gmail.com>
2023-03-11 15:07:22 +0800

    assert_almost_equal

--- test/src/assert_equal.hpp
+++ test/src/assert_equal.hpp
@@ -1,8 +1,28 @@
 
 #pragma once
 
+#include <cmath>
 #include <cstdlib>
 #include <iostream>
+#include <limits>
+
+template<class T>
+struct approx
+{
+    T v;
+    constexpr approx(T _v) noexcept
+        : v(_v)
+    {
+    }
+    bool operator==(approx rhs) const
+    {
+        return std::abs(v - rhs.v) < 1e-7;
+    }
+    friend std::ostream& operator<<(std::ostream& os, approx x)
+    {
+        return os << x.v;
+    }
+};
 
 #define assert_equal(lhs, rhs) \
     do { \
@@ -14,3 +34,5 @@
                   << "\n"; \
         std::exit(EXIT_FAILURE); \
     } while (0)
+
+#define assert_almost_equal(lhs, rhs) assert_equal(approx(lhs), approx(rhs))
